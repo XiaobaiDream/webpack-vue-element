e1b9f7aab77378d5974fded0ac5186f9c85d400a
zhoukai <zhoukai@globalegrow.com>
2018-10-31 13:56:44 +0800

    增加首页

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <my-component></my-component>
+        {{message}}
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                message: 'Hello World!'
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
